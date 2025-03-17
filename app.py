4744fe1d42a76b31eacf1ca84e7b0082c212f286
martin-arcpi <104268393+martin-arcpi@users.noreply.github.com>
2025-03-17 12:49:08 +0000

    Add more functionality.

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from nltk.corpus import brown
+from random import randint, shuffle, choice
 
 word_list = list(set(brown.words()))
+words = []
 
-print(word_list[0:10])
+for i in range(280):
+    words.append(choice(word_list))
 
+print(words)
 
