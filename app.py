9836bacf7a7d967d6767c96e980f042c49485a51
martin-arcpi <104268393+martin-arcpi@users.noreply.github.com>
2025-03-18 12:46:07 +0000

    Add paragraph generation capability.

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from nltk.corpus import brown
 from random import randint, shuffle, choice
 
-word_list = list(set(brown.words()))
-words = []
 
-for i in range(280):
-    words.append(choice(word_list))
+# TODO add varying length sentences using a for loop to create a paragraph simulating a twitter message.
 
-print(words)
+
+def generate_text():
+    word_list = list(set(brown.words()))
+    words = [choice(word_list) for i in range(280)]
+
+    sentences = []
+    text = ''
+
+    for i in range(20):
+        for j in range(randint(1, 10)):
+            text += choice(words)
+            text += ' '
+        sentences.append(text)
+        text = ''
+
+    print(sentences )
+            
+    
+
+print(generate_text())
+
 
